6a83d42c969ad7dcaee3b673d28674b2b51f45a4
Max <maksymilian@optime.ai>
2024-04-08 12:41:07 +0200

    Chanaged schema ships

--- routers/ships.py
+++ routers/ships.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from fastapi import HTTPException, Path
 from typing import Dict
 
-from schemas.ships import Starship
+from schemas.ships import StarshipSchemaRecived, StarshipSchemaStored
 
 router = APIRouter(prefix='/starships', tags=['starships'])
 
@@ -10,37 +10,47 @@ router = APIRouter(prefix='/starships', tags=['starships'])
 
 # Mock Database
 starships_db = {
-    1: {"name": "Millennium Falcon", "price_per_hour": 1000, "available": True},
-    2: {"name": "X-wing", "price_per_hour": 800, "available": True},
-    3: {"name": "Star Destroyer", "price_per_hour": 5000, "available": False},
+    1: {"name": "Millennium Falcon", "price_per_hour": 1000, "available": True, "max_range":1000, "max_speed":1000, "id":1},
+    2: {"name": "X-wing", "price_per_hour": 800, "available": True, "max_range":800, "max_speed":200, "id":2},
+    3: {"name": "Star Destroyer", "price_per_hour": 5000, "available": False, "max_range":100, "max_speed":1, "id":3},
 }
 
 
 
 
-@router.get("/", response_model=Dict[int, Starship])
-async def list_starships() -> Starship:
+@router.get("/", response_model=Dict[int, StarshipSchemaStored])
+async def list_starships() -> StarshipSchemaStored:
     return starships_db
 
-@router.get("/{starship_id}", response_model=Starship)
+@router.get("/{starship_id}", response_model=StarshipSchemaStored)
 async def get_starship(starship_id: int = Path(..., title="The ID of the starship to get")):
     if starship_id not in starships_db:
         raise HTTPException(status_code=404, detail="Starship not found")
-    return starships_db[starship_id]
+    starship_data = starships_db[starship_id]
+    starship_data["id"]=starship_id
+    return starship_data
 
-@router.post("/", response_model=Starship, status_code=201)
-async def create_starship(starship: Starship):
+@router.post("/", response_model=StarshipSchemaRecived, status_code=201)
+async def create_starship(starship: StarshipSchemaRecived):
     new_id = max(starships_db.keys()) + 1
-    new_starship = Starship(id=new_id, name=starship.name, available=True, price_per_hour=starship.price_per_hour)
+    new_starship = StarshipSchemaStored(id=new_id, name=starship.name, available=True, price_per_hour=starship.price_per_hour, max_speed =starship.max_speed, max_range=starship.max_range)
     starships_db[new_id] = new_starship.dict()
     return starship
 
-@router.put("/{starship_id}", response_model=Starship)
-async def update_starship(starship_id: int, starship: Starship):
+@router.put("/{starship_id}", response_model=StarshipSchemaStored)
+async def update_starship(starship_id: int, starship: StarshipSchemaRecived):
     if starship_id not in starships_db:
         raise HTTPException(status_code=404, detail="Starship not found")
-    starships_db[starship_id] = starship.dict()
-    return starship
+    updated_starship = StarshipSchemaStored(
+        id=starship_id,
+        name=starship.name,
+        price_per_hour=starship.price_per_hour,
+        available=starship.available,
+        max_range=starship.max_range,
+        max_speed=starship.max_speed
+    )
+    starships_db[starship_id] = updated_starship.dict()
+    return updated_starship
 
 @router.delete("/{starship_id}", response_model=None)
 async def delete_starship(starship_id: int):
--- schemas/ships.py
+++ schemas/ships.py
@@ -1,6 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PositiveInt, PositiveFloat, Field
+from typing import Optional
 
-class Starship(BaseModel):
-    name: str
-    price_per_hour: float
+class StarshipSchemaRecived(BaseModel):
+    name: str = Field(min_length=5, max_length=255)
+    price_per_hour: PositiveFloat
     available: bool
+    max_speed: PositiveInt
+    max_range: PositiveInt
+
+class StarshipSchemaStored(BaseModel):
+    name: str = Field(min_length=5, max_length=255)
+    price_per_hour: PositiveFloat
+    available: bool
+    id: PositiveInt
+    max_speed: PositiveInt
+    max_range: PositiveInt
+
