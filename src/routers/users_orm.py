f32324bddf4718103fb9cc26bb2137df5e8bbf84
Max <maksymilian@optime.ai>
2024-04-16 15:57:18 +0200

    Dockstring, move secret to env and env sample

--- src/routers/users_orm.py
+++ src/routers/users_orm.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime, timedelta
 
 from fastapi import APIRouter, Depends, HTTPException, status
@@ -17,31 +18,57 @@ router = APIRouter(
 )
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
-# TODO Move to env
-SECRET_KEY = "620191445bbb51e8f590a53cb80b8a9b8a818fe6f5be22c3b9d5a3c078078b3e"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+# get env from file
+SECRET_KEY = os.getenv("SECRET_KEY", "TEST")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 def verify_password(plain_password, hashed_password):
+    """
+    Verify if the provided plain password matches the hashed password.
+
+    Args:
+        plain_password (str): The plain text password to be verified.
+        hashed_password (str): The hashed password to be compared against.
+
+    Returns:
+        bool: True if the plain password matches the hashed password, False otherwise.
+    """
     return pwd_context.verify(plain_password, hashed_password)
 
 
 def get_password_hashed(password):
+    """
+    Hash the provided password.
+
+    Args:
+        password (str): The password to be hashed.
+
+    Returns:
+        str: The hashed password.
+    """
     return pwd_context.hash(password)
 
 
 class UserInDB(UserSchemaStored):
+    """A representation of a user stored in the database."""
+
     hashed_password: str
 
 
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    Create an access token.
+
+    Args:
+        data (dict): The payload data to be encoded into the token.
+        expires_delta (timedelta, optional): The duration of time before the token expires.
+            Defaults to None.
+
+    Returns:
+        str: The encoded access token.
+    """
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -53,21 +80,37 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 
 
 def authenticate_user(db, username: str, password: str):
+    """Authenticate a user."""
     user = user_repo.get_user_by_identifier(db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
+    if not user or not verify_password(password, user.hashed_password):
         return False
     return user
 
 
 class Token(BaseModel):
+    """Token model."""
+
     access_token: str
     token_type: str
 
 
 @router.post("/token")
 async def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
+    """
+       Get an access token by logging in.
+
+       Authenticates a user with the provided credentials and returns an access token if successful.
+
+       Args:
+           db (Session, optional): The database session. Defaults to Depends(get_db).
+           form_data (OAuth2PasswordRequestForm, optional): The form data containing the user's credentials. Defaults to Depends().
+
+       Raises:
+           HTTPException: If the provided username or password is incorrect.
+
+       Returns:
+           Token: An access token.
+       """
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
@@ -76,16 +119,14 @@ async def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
     return Token(access_token=access_token, token_type="bearer")
 
 
 @router.post("/", response_model=UserSchemaStored, status_code=201)
 async def create_user(user: UserSchemaReceived, db: Session = Depends(get_db)):
     """
-    Endpoint to create a new user.
+    Create a new user.
 
     Args:
         user (UserSchemaReceived): User data to be created.
@@ -102,6 +143,5 @@ async def create_user(user: UserSchemaReceived, db: Session = Depends(get_db)):
         full_name=user.full_name,
         email=user.email,
         active_user=True,
-
     )
     return new_user
